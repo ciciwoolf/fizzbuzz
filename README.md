04d5f97cd2da5d863f300e6511bff3b62dceae1d
Cici Woolf <christinemarie.woolf@gmail.com>
2022-06-05 10:47:49 -0500

    Add variation

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # The FizzBuzz Challenge
-> Here is a simple solution for the fizzbuzz challenge. There are a variety of ways to complete this exercise but this is the one that comes to mind first. 
+> There are a variety of ways to complete this exercise but the solution (with a variation) I've given here came to mind first and took under the 15-20 minutes suggested for this exercise. 
+
+> In a workplace context, I would likely ask a coworker for a code review and ask for ways to refactor the code using ternary operators, etc. Or, I would take more time to refactor it myself after researching different methods online.
+
+> Thank you for considering me for this position and taking the time to review this application.
 
 >-Christine ("Cici") Woolf
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,8 @@
 /**
-  * Write a program that prints the number from 1 - 100. But for multiples of three print "fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print  "FizzBuzz".
+  * Write a program that prints the number from 1 - 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print  "FizzBuzz".
  */
 
-// Straightforward Loop with if conditionals
+// Straightforward Loop with if, else if conditionals
 
 for(let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
@@ -14,5 +14,23 @@ for(let i = 1; i <= 100; i++) {
     } else {
         console.log(i);
     }
+}
+
+// Organizing with const declarations first
+
+for(let i = 1; i <= 100; i++) {
+    const FizzBuzz = i % 3 === 0 && i % 5 === 0;
+    const Fizz = i % 3 === 0;
+    const Buzz = i % 5 === 0;
+    
+    if (FizzBuzz) {
+        console.log("FizzBuzz");
+    } else if (Fizz) {
+        console.log("Fizz");
+    } else if (Buzz) {
+        console.log("Buzz")
+    } else {
+        console.log(i);
+    }    
 }
 
